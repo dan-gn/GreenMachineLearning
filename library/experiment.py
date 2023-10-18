ff5dc3ec41c0996615fe8dd74b831cfd54a6b0e5
dan-gn <78847525+dan-gn@users.noreply.github.com>
2023-10-18 15:38:53 +0100

    Subsampling and pca options added to pipeline

--- library/experiment.py
+++ library/experiment.py
@@ -1,11 +1,15 @@
+# Utilities libraries
+import numpy as np
+import types
+
 # Machine learning pipeline libraries
-from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 # Metrics libraries
 import time
 from sklearn.metrics import accuracy_score, log_loss
-import types
 from sklearn import metrics 
 
 # Machine Learning models
@@ -13,117 +17,155 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier        
 from kdnn import KDNN
-import numpy as np
 
 # Utilities
 def is_instance_attr(obj, name):
-  if not hasattr(obj, name):
-    return False
-  if name.startswith("__") and name.endswith("__"):
-    return False
-  v = getattr(obj, name)
-  if isinstance(v, (types.BuiltinFunctionType, types.BuiltinMethodType, types.FunctionType, types.MethodType)):
-    return False
-  # See https://stackoverflow.com/a/17735709/
-  attr_type = getattr(type(obj), name, None)
-  if isinstance(attr_type, property):
-    return False
-  return True
+   if not hasattr(obj, name):
+      return False
+   if name.startswith("__") and name.endswith("__"):
+      return False
+   v = getattr(obj, name)
+   if isinstance(v, (types.BuiltinFunctionType, types.BuiltinMethodType, types.FunctionType, types.MethodType)):
+      return False
+   # See https://stackoverflow.com/a/17735709/
+   attr_type = getattr(type(obj), name, None)
+   if isinstance(attr_type, property):
+      return False
+   return True
 
 def get_instance_attrs(obj):
-  names = dir(obj)
-  names = [name for name in names if is_instance_attr(obj, name)]
-  return names
+   names = dir(obj)
+   names = [name for name in names if is_instance_attr(obj, name)]
+   return names
 
 def sklearn_sizeof(obj):
-  sum = 0
-  names = get_instance_attrs(obj)
-  for name in names:
-    v = getattr(obj, name)
-    v_type = type(v)
-    v_sizeof = v.__sizeof__()
-    sum += v_sizeof
-  return sum
+   sum = 0
+   names = get_instance_attrs(obj)
+   for name in names:
+      v = getattr(obj, name)
+      v_type = type(v)
+      v_sizeof = v.__sizeof__()
+      sum += v_sizeof
+   return sum
 
+# Constants
+N_FOLDS = 10
+N_REPEATS = 2 # CHANGE THIS TO 3 FOR FINAL EXPERIMENTS
 
 """
 Experiment
 """
 class Experiment:
 
-    def __init__(self, model_name, X, y) -> None:
-        self.model_name = model_name
-        self.X = X
-        self.y = y
-        self.n_folds = 10
-        self.n_repeats = 2
-        self.random_state = 0
-        self.results = []
+   def __init__(self, model_name, X, y, subsampling = False, feature_reduction = False, random_state = 0) -> None: # CHANGE random_state TO None
+      self.model_name = model_name
+      self.X = X
+      self.y = y
+      self.n_folds = N_FOLDS
+      self.n_repeats = N_REPEATS
+      self.random_state = random_state
+      self.subsampling = subsampling
+      self.subsamplig_frac = 0.2
+      self.feature_reduction = feature_reduction
+      self.results = []
 
-    def preprocess_data(self, X_train, X_test):
-        scaler = StandardScaler()
-        X_train_scaled = scaler.fit_transform(X_train)
-        X_test_scaled = scaler.transform(X_test)
-        return X_train_scaled, X_test_scaled
+   def do_subsampling(self, X_train:np.ndarray, y_train: np.ndarray):
+      if self.subsampling:
+         X_train_sub, y_train_sub = [], []
+         for label in range(2): # 2 cause we are doing binary classification
+            X_train_label = X_train[y_train == label]
+            n_samples = round(X_train_label.shape[0] * self.subsamplig_frac)
+            index = np.random.choice(X_train_label.shape[0], n_samples, replace=False)
+            X_train_sub.extend([x for i, x in enumerate(X_train_label) if i in index])
+            y_train_sub.extend([label] * n_samples)
+         X_train_pp, y_train_pp = np.array(X_train_sub), np.array(y_train_sub)
+      return X_train_pp, y_train_pp
+
+   def do_pca(self, X_train: np.ndarray, X_test: np.ndarray):
+      pca = PCA(n_components=2)
+      X_train_pp = pca.fit_transform(X_train)
+      X_test_pp = pca.transform(X_test)
+      return X_train_pp, X_test_pp
+   
+   def do_data_scaling(self, X_train:np.ndarray, X_test:np.ndarray):
+      scaler = StandardScaler()
+      X_train_scaled = scaler.fit_transform(X_train)
+      X_test_scaled = scaler.transform(X_test)
+      return X_train_scaled, X_test_scaled
+
+   def preprocess_data(self, X_train: np.ndarray, X_test: np.ndarray, y_train: np.ndarray):
+      if self.subsampling:
+         X_train_pp, y_train_pp = self.do_subsampling(X_train, y_train)
+      else:
+         X_train_pp, y_train_pp = X_train, y_train
+      if self.feature_reduction:
+         X_train_pp, X_test_pp = self.do_pca(X_train_pp, X_test)
+      else:
+         X_test_pp = X_test
+      X_train_scaled, X_test_scaled = self.do_data_scaling(X_train_pp, X_test_pp)
+      return X_train_scaled, X_test_scaled, y_train_pp
     
-    def new_model(self):
-        if self.model_name == 'LogisticRegression':
-           return LogisticRegression(random_state=0)
-        if self.model_name == 'SVC':
-           return SVC(random_state=0)
-        elif self.model_name == 'RandomForestClassifier':
-           return RandomForestClassifier(random_state=0)
-        elif self.model_name == 'GradientBoostingClassifier':
-           return GradientBoostingClassifier(random_state=0)
-        elif self.model_name == 'DeepNeuralNetwork':
-           return KDNN.kdnn_model()
-        else:
-           print('ML model not supported')
-    
-    def run(self):
-        # Repeated K-Fold Cross Validation
-        rkf = RepeatedKFold(n_splits=self.n_folds, n_repeats=self.n_repeats, random_state=self.random_state)
-        for i, (train_index, test_index) in enumerate(rkf.split(self.X)):
+   def new_model(self):
+      if self.model_name == 'LogisticRegression':
+         return LogisticRegression(random_state=self.random_state)
+      if self.model_name == 'SVC':
+         return SVC(random_state=self.random_state)
+      elif self.model_name == 'RandomForestClassifier':
+         return RandomForestClassifier(random_state=self.random_state)
+      elif self.model_name == 'GradientBoostingClassifier':
+         return GradientBoostingClassifier(random_state=self.random_state)
+      elif self.model_name == 'DeepNeuralNetwork':
+         return KDNN.kdnn_model()
+      else:
+         print('ML model not supported')
+   
+   def run(self):
+      # Repeated K-Fold Cross Validation
+      rkf = RepeatedStratifiedKFold(n_splits=self.n_folds, n_repeats=self.n_repeats, random_state=self.random_state)
+      for i, (train_index, test_index) in enumerate(rkf.split(self.X, self.y)):
 
-            # Split data
-            X_train, X_test = self.X[train_index], self.X[test_index]
-            y_train, y_test = self.y[train_index], self.y[test_index]
+         # Split data
+         X_train, X_test = self.X[train_index], self.X[test_index]
+         y_train, y_test = self.y[train_index], self.y[test_index]
 
-            # Preprocess data
-            X_train_scaled, X_test_scaled = self.preprocess_data(X_train, X_test)
+         # Preprocess data
+         start = time.time()
+         X_train_pp, X_test_pp, y_train_pp = self.preprocess_data(X_train, X_test, y_train)
+         preprocess_time = time.time() - start
 
-            self.model = self.new_model()
+         self.model = self.new_model()
 
-            # Train model
-            start = time.time()
-            self.model.fit(X_train_scaled, y_train)
-            training_time = time.time() - start
+         # Train model
+         start = time.time()
+         self.model.fit(X_train_pp, y_train_pp)
+         training_time = time.time() - start
 
-            # Evaluate model
-            start = time.time()
-            y_pred = self.model.predict(X_test_scaled)
-            prediction_time = time.time() - start       # SHOULD WE NORMALIZE THIS BY UNIT???
-            #ROC curve parameters: false positive rate (fpr) and true positive rate (tpr)
-            fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
-            #AUC
-            auc = metrics.auc(fpr, tpr)
+         # Evaluate model
+         start = time.time()
+         y_pred = self.model.predict(X_test_pp)
+         prediction_time = time.time() - start       # SHOULD WE NORMALIZE THIS BY UNIT???
+         #ROC curve parameters: false positive rate (fpr) and true positive rate (tpr)
+         fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
+         #AUC
+         auc = metrics.auc(fpr, tpr)
 
-            # Store results
-            measures = {
-                'accuracy': accuracy_score(y_test, np.round(y_pred)),
-                'log_loss': log_loss(y_test, y_pred),
-                'fpr': fpr,
-                'tpr': tpr,
-                'auc': auc,
-                'training_time': training_time,
-                'prediciton_time': prediction_time,
-                'model_size': sklearn_sizeof(self.model)
-            }
-            self.results.append(measures)
+         # Store results
+         measures = {
+               'accuracy': accuracy_score(y_test, np.round(y_pred)),
+               'log_loss': log_loss(y_test, y_pred),
+               'fpr': fpr,
+               'tpr': tpr,
+               'auc': auc,
+               'preprocess_time': preprocess_time,
+               'training_time': training_time,
+               'prediciton_time': prediction_time,
+               'model_size': sklearn_sizeof(self.model)
+         }
+         self.results.append(measures)
 
-        return self.results
-    
-    def get_mesaure(self, measure):
-        return [x[measure] for x in self.results]
+      return self.results
+   
+   def get_mesaure(self, measure):
+      return [x[measure] for x in self.results]
 
-    
+   
