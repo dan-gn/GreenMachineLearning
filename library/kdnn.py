6ae73258a51664f67f688efd12e7281592c235d9
Samara Santos <oss.samara@gmail.com>
2023-11-06 11:55:52 +0000

    new kdnn

--- library/kdnn.py
+++ library/kdnn.py
@@ -1,21 +1,29 @@
 from keras import optimizers
 from keras.models import Sequential
-from keras.layers import Dense, SimpleRNN
-import tensorflow as tf
+from keras.layers import Dense
 
 class KDNN():
 
     def kdnn_model():
         model = Sequential()
-        model.add(Dense(64, activation='relu'))
-        # model.add(SimpleRNN(64, activation="relu"))
-        # model.add(Dense(1536, activation='relu'))
-        # model.add(Dense(64, activation='relu')) 
-        # model.add(Dense(64, activation='relu')) 
-        # model.add(Dense(256, activation='relu')) 
-        model.add(Dense(128, activation='relu')) 
-        model.add(Dense(64, activation='relu')) 
+
+        # First Layer
+        model.add(Dense(64, activation='sigmoid'))
+
+        # Second Layer
+        model.add(Dense(1536, activation='sigmoid'))
+
+        # Third Layer
+        model.add(Dense(64, activation='sigmoid'))
+        model.add(Dense(64, activation='sigmoid'))
+
+        # Fourth Layer
+        # model.add(Dense(256, activation='relu'))
+        model.add(Dense(128, activation='sigmoid')) 
+        model.add(Dense(64, activation='sigmoid'))
+
         model.add(Dense(1, activation='sigmoid'))
         opt = optimizers.Adam(learning_rate=0.03)
         model.compile(loss='binary_crossentropy', optimizer= opt, metrics=['accuracy'])
+
         return model
