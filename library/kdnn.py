aa04e1c299ec97d08f035e76cf10799233de6e5d
Samara Santos <oss.samara@gmail.com>
2023-11-06 13:34:11 +0000

    new kdnn lib

--- library/kdnn.py
+++ library/kdnn.py
@@ -2,28 +2,38 @@ from keras import optimizers
 from keras.models import Sequential
 from keras.layers import Dense
 
-class KDNN():
-
+class KDNN_v1():
+ 
     def kdnn_model():
         model = Sequential()
-
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dense(64, activation='relu'))
+        model.compile(loss='binary_crossentropy', optimizer= "adam", metrics=['accuracy'])
+        return model
+ 
+class KDNN_v2():
+ 
+    def kdnn_model():
+        model = Sequential()
+ 
         # First Layer
         model.add(Dense(64, activation='sigmoid'))
-
+ 
         # Second Layer
         model.add(Dense(1536, activation='sigmoid'))
-
+ 
         # Third Layer
         model.add(Dense(64, activation='sigmoid'))
         model.add(Dense(64, activation='sigmoid'))
-
+ 
         # Fourth Layer
         # model.add(Dense(256, activation='relu'))
-        model.add(Dense(128, activation='sigmoid')) 
+        model.add(Dense(128, activation='sigmoid'))
         model.add(Dense(64, activation='sigmoid'))
-
+ 
         model.add(Dense(1, activation='sigmoid'))
         opt = optimizers.Adam(learning_rate=0.03)
         model.compile(loss='binary_crossentropy', optimizer= opt, metrics=['accuracy'])
-
+ 
         return model
